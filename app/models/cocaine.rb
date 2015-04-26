a6b8fe7f734dd8e9feb8058072683639826f266d
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-04-25 22:04:09 -0300

    Populando a model Cocaine
    Signed-off-by: Kleber Brito <kleberbritomoreira10@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- app/models/cocaine.rb
+++ app/models/cocaine.rb
@@ -1,7 +1,6 @@
 class Cocaine < ActiveRecord::Base
-  arq = File.new("DrugsTypes.txt", "r")
-
-  a = File.readlines("DrugsTypes.txt")
+	  	
 end
+
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    data = YAML::load_file(File.join(Rails.root,"public/static/cocaina.yml"))
+
+    cocaine = Cocaine.new
+
+    cocaine.name = data["Nome"]
+    cocaine.origin = data["Origem"]
+    cocaine.average_ingest = data["Quantidade_media_ingerida"]
+    cocaine.ingestion_form = data["Forma_ingestao"]
+    cocaine.effect_average = data["Efeitos_a_curto_prazo_grandes_quantidades"]
+    cocaine.effect_high = data["Risco_de_dependencia_psicologica"]
+    cocaine.dependence_psychological = data["Risco_de_dependencia_fisica"]
+    cocaine.dependence_phisical = data["Tolerancia"]
+    cocaine.tolerance = data["Efeitos_a_longo_prazo"]
+    cocaine.high_term = data["Utilizacao_medica"]
+    cocaine.long_effects = data["Efeitos_a_longo_prazo"]
+    cocaine.medical_use = data["Utilizacao_medica"] 
+    
+    cocaine.save!
