671e51fb5ae100ed0078ee19e9bf47b21012b56b
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-04-26 14:26:36 -0300

    Adicionando campos para as tabelas de migração
    Signed-off-by: Kleber Brito <kleberbritomoreira10@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- db/migrate/20150426161231_create_narcotics.rb
+++ db/migrate/20150426161231_create_narcotics.rb
@@ -1,7 +1,18 @@
 class CreateNarcotics < ActiveRecord::Migration
   def change
     create_table :narcotics do |t|
-
+      t.string :name
+      t.string :origin
+      t.string :average_ingest
+      t.string :ingestion_form
+      t.string :effect_average
+      t.string :effect_high
+      t.string :dependence_psychological
+      t.string :dependence_phisical
+      t.string :tolerance
+      t.string :high_term
+      t.string :long_effects
+      t.string :medical_use	
       t.timestamps null: false
     end
   end
